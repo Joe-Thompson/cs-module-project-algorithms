4b285730018d684129fb55e0f13b5895fdf9a07c
Joe-Thompson <bmo3696@gmail.com>
2020-09-09 16:06:16 -0400

    finished single_number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -1,11 +1,21 @@
-'''
+"""
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
-'''
-def single_number(arr):
-    # Your code here
+"""
 
-    pass
+
+def single_number(arr):
+    if len(arr) <= 1:
+        return arr
+    print(f"this is the arr: {arr}")
+    starter = arr[0]
+    print(f"this is the end_pointer: {starter}")
+    for num in arr[1:]:
+        if num == starter:
+            arr.remove(num)
+            arr.remove(starter)
+            single_number(arr)
+    return arr[0]
 
 
 if __name__ == '__main__':
